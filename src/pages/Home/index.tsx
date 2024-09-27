63170a8beda18d5e7504ad8a05b5829da5a79f2f
Amir Shahzad <112079597+Amir00786@users.noreply.github.com>
2024-09-27 11:27:03 +0500

    style(generator in action): generator in action styles

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,11 @@
 import { Banner, Pricing } from '@/pages/home/components';
+import GeneratorDetail from '../../layouts/GeneratorDetail';
 
 const Home = () => {
   return (
     <div>
       <Banner />
+      <GeneratorDetail />
       <Pricing />
     </div>
   );
