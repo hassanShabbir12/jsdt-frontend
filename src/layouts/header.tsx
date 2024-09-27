c17702c5d6b87cf39c3c63959ce8c69faf1585dc
awaisali90 <awais_ali90@hotmail.com>
2024-09-27 12:41:45 +0500

    style(banner): banner strcture update

--- src/layouts/header.tsx
+++ src/layouts/header.tsx
@@ -1,13 +1,16 @@
 import { Button } from '@/components/ui/button';
 import ImageLogo from '@/assets/images/logo.png';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <header className='relative bg-white px-4 py-4 text-black lg:py-5'>
-      <div className='container mx-auto'>
-        <div className='relative flex flex-wrap items-center justify-between'>
-          <div className='w-20 md:w-28'>
-            <img className='w-full' src={ImageLogo} alt='JSDT Examiner' />
+    <header className='relative bg-white p-4'>
+      <div className='container lg:py-0.5'>
+        <div className='relative flex flex-wrap items-center justify-between text-black'>
+          <div className='w-20 md:w-24'>
+            <Link to='/'>
+              <img className='w-full' src={ImageLogo} alt='JSDT Examiner' />
+            </Link>
           </div>
           <Button variant='secondary' size='lg'>
             Sign Up Now
