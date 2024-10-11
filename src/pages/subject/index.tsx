1b951e600f0544efb13de87ff96ee8c2fb3ad40a
Amirdev88 <aamirraz182@gmail.com>
2024-10-11 13:54:04 +0500

    style(dropdown): dropdown styles

--- src/pages/subject/index.tsx
+++ src/pages/subject/index.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect, useRef, useState } from 'react';
+import { FC } from 'react';
 
 import { Edit, Trash2 } from 'lucide-react';
 
@@ -12,111 +12,90 @@ import {
   DialogTitle,
   DialogTrigger,
 } from '@/components/ui/dialog';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 
-export const Subjects: FC = () => {
-  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-  const dropdownRef = useRef<HTMLDivElement>(null);
-
-  const toggleDropdown = (): void => {
-    setIsDropdownOpen(!isDropdownOpen);
-  };
-
-  useEffect(() => {
-    const handleClickOutside = (event: MouseEvent): void => {
-      if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
-        setIsDropdownOpen(false);
-      }
-    };
-
-    document.addEventListener('mousedown', handleClickOutside);
-
-    return (): void => {
-      document.removeEventListener('mousedown', handleClickOutside);
-    };
-  }, []);
-
-  return (
-    <div className='px-6 pt-24 md:pl-0 md:pr-6 md:pt-16'>
-      <div className='rounded-md bg-white pb-7 shadow-lg'>
-        <div className='border-b border-neutral-200 px-6 py-3'>
-          <h1 className='text-lg font-semibold text-zinc-800'>Subjects</h1>
+export const Subjects: FC = () => (
+  <div className='px-6 pt-24 md:pl-0 md:pr-6 md:pt-16'>
+    <div className='rounded-md bg-white pb-7 shadow-lg'>
+      <div className='border-b border-neutral-200 px-6 py-3'>
+        <h1 className='text-lg font-semibold text-zinc-800'>Subjects</h1>
+      </div>
+      <div className='p-4 sm:px-6 sm:pb-3 sm:pt-8'>
+        <Label
+          htmlFor='name'
+          className='mb-1.5 block text-sm font-normal leading-none text-zinc-800'
+        >
+          Subject Name
+        </Label>
+        <div className='flex flex-wrap items-center'>
+          <Input
+            id='name'
+            className='h-10 w-auto min-w-0 grow basis-0 rounded-lg border-neutral-200 px-4 text-base text-zinc-800 shadow-none placeholder:text-zinc-800 lg:h-12 lg:px-3 lg:py-2'
+            placeholder='Chemistry'
+          />
+          <DropdownMenu>
+            <DropdownMenuTrigger className='ml-4 flex h-8 w-8 items-center justify-center rounded-full border border-zinc-800 hover:bg-zinc-800 hover:text-white data-[state=open]:bg-zinc-800 data-[state=open]:text-white'>
+              <GradeDots />
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className='absolute -right-4 top-4 rounded-md drop-shadow-xl'>
+              <ul className='m-0 px-2 py-3'>
+                <li className='mb-2 flex cursor-pointer items-center p-1.5'>
+                  <div className='text-zinc-800'>
+                    <Edit />
+                  </div>
+                  <h3 className='ml-1.5 text-base text-zinc-800'>Edit</h3>
+                </li>
+                <li className='flex cursor-pointer items-center p-1.5'>
+                  <div className='text-zinc-800'>
+                    <Trash2 />
+                  </div>
+                  <h3 className='ml-1.5 text-base text-zinc-800'>Delete</h3>
+                </li>
+              </ul>
+            </DropdownMenuContent>
+          </DropdownMenu>
         </div>
-        <div className='p-4 sm:px-6 sm:pb-3 sm:pt-8'>
-          <Label
-            htmlFor='name'
-            className='mb-1.5 block text-sm font-normal leading-none text-zinc-800'
-          >
-            Subject Name
-          </Label>
-          <div className='flex items-center'>
-            <Input
-              id='name'
-              className='h-10 rounded-lg border-neutral-200 px-4 text-base text-zinc-800 shadow-none placeholder:text-zinc-800 lg:h-12 lg:px-3 lg:py-2'
-              placeholder='Chemistry'
-            />
-            <div className='relative' ref={dropdownRef}>
-              <div
-                className='ml-4 flex h-8 w-8 cursor-pointer items-center justify-center rounded-full border border-zinc-800'
-                onClick={toggleDropdown}
-              >
-                <GradeDots />
-              </div>
-              {isDropdownOpen && (
-                <ul className='absolute -bottom-28 right-0 m-0 w-36 rounded-md bg-white p-3 shadow-2xl'>
-                  <li className='flex cursor-pointer items-center p-1.5'>
-                    <div className='text-zinc-800'>
-                      <Edit />
-                    </div>
-                    <h3 className='ml-1.5 text-base text-zinc-800'>Edit</h3>
-                  </li>
-                  <li className='flex cursor-pointer items-center p-1.5'>
-                    <div className='text-zinc-800'>
-                      <Trash2 />
-                    </div>
-                    <h3 className='ml-1.5 text-base text-zinc-800'>Delete</h3>
-                  </li>
-                </ul>
-              )}
+      </div>
+      <Dialog>
+        <DialogTrigger asChild>
+          <div className='mx-auto flex max-w-80 justify-center px-4'>
+            <Button className='h-12 w-80 text-base font-semibold'>Add New Subject</Button>
+          </div>
+        </DialogTrigger>
+        <DialogContent className='max-w-[620px]'>
+          <DialogHeader>
+            <DialogTitle className='text-center'>Add New Subject</DialogTitle>
+          </DialogHeader>
+          <div className='grid py-4'>
+            <Label className='mb-2 block text-sm font-normal leading-none text-zinc-800'>
+              New Subject
+            </Label>
+            <div className='flex items-center'>
+              <Input
+                id='name'
+                className='h-10 rounded-lg border-neutral-200 px-4 text-base text-zinc-800 shadow-none placeholder:text-stone-300 lg:h-12 lg:px-3 lg:py-2'
+                placeholder='Type here'
+              />
             </div>
           </div>
-        </div>
-        <Dialog>
-          <DialogTrigger asChild>
-            <div className='mx-auto flex max-w-80 justify-center px-4'>
-              <Button className='h-12 w-80 text-base font-semibold'>Add New Subject</Button>
+          <DialogFooter>
+            <div className='w-1/2'>
+              <Button variant='outline' className='h-12 w-full text-base font-semibold'>
+                Cancel
+              </Button>
             </div>
-          </DialogTrigger>
-          <DialogContent className='max-w-[620px]'>
-            <DialogHeader>
-              <DialogTitle className='text-center'>Add New Subject</DialogTitle>
-            </DialogHeader>
-            <div className='grid py-4'>
-              <Label className='mb-2 block text-sm font-normal leading-none text-zinc-800'>
-                New Subject
-              </Label>
-              <div className='flex items-center'>
-                <Input
-                  id='name'
-                  className='h-10 rounded-lg border-neutral-200 px-4 text-base text-zinc-800 shadow-none placeholder:text-stone-300 lg:h-12 lg:px-3 lg:py-2'
-                  placeholder='Type here'
-                />
-              </div>
+            <div className='w-1/2'>
+              <Button className='h-12 w-full text-base font-semibold'>Save</Button>
             </div>
-            <DialogFooter>
-              <div className='w-1/2'>
-                <Button variant='outline' className='h-12 w-full text-base font-semibold'>
-                  Cancel
-                </Button>
-              </div>
-              <div className='w-1/2'>
-                <Button className='h-12 w-full text-base font-semibold'>Save</Button>
-              </div>
-            </DialogFooter>
-          </DialogContent>
-        </Dialog>
-      </div>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
     </div>
-  );
-};
+  </div>
+);
