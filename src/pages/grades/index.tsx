9e7bffba2b36163a47bfb8feac871558e4a2149d
Amir Shahzad <112079597+Amir00786@users.noreply.github.com>
2024-11-05 16:35:25 +0500

    style(add table in grades page): add table in grades page styles

--- src/pages/grades/index.tsx
+++ src/pages/grades/index.tsx
@@ -19,82 +19,142 @@ import {
 } from '@/components/ui/dropdown-menu';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
+
+const invoices = [
+  {
+    invoice: 'Grade 1',
+    icon: <Trash2 />,
+  },
+  {
+    invoice: 'Grade 2',
+    icon: <Trash2 />,
+  },
+  {
+    invoice: 'Grade 3',
+    icon: <Trash2 />,
+  },
+  {
+    invoice: 'Grade 4',
+    icon: <Trash2 />,
+  },
+];
 
 export const Grades: FC = () => (
-  <div className='px-6 pt-24 md:pl-0 md:pr-6 md:pt-16'>
-    <div className='rounded-md bg-white pb-7 shadow-lg'>
-      <div className='border-b border-neutral-200 px-6 py-3'>
-        <h1 className='text-lg font-semibold text-zinc-800'>Grades</h1>
-      </div>
-      <div className='p-4 sm:px-6 sm:pb-3 sm:pt-8'>
-        <Label htmlFor='name' className='mb-2 block text-sm font-normal leading-none text-zinc-800'>
-          Grade
-        </Label>
-        <div className='flex flex-wrap items-center'>
-          <Input
-            id='name'
-            className='h-10 w-auto min-w-0 grow basis-0 rounded-lg border-neutral-200 px-4 text-base text-zinc-800 shadow-none placeholder:text-zinc-800 lg:h-12 lg:px-3 lg:py-2'
-            placeholder='Grade 1'
-          />
-          <DropdownMenu>
-            <DropdownMenuTrigger className='ml-4 flex h-8 w-8 items-center justify-center rounded-full border border-zinc-800'>
-              <GradeDots />
-            </DropdownMenuTrigger>
-            <DropdownMenuContent className='absolute -right-4 top-4 rounded-md drop-shadow-xl'>
-              <ul className='m-0 px-2 py-3'>
-                <li className='group mb-2 flex cursor-pointer items-center p-1.5'>
-                  <div className='text-zinc-800 group-hover:text-blue-500'>
-                    <Edit />
-                  </div>
-                  <h3 className='ml-1.5 text-base text-zinc-800 group-hover:text-blue-500'>Edit</h3>
-                </li>
-                <li className='group flex cursor-pointer items-center p-1.5'>
-                  <div className='text-zinc-800 group-hover:text-blue-500'>
-                    <Trash2 />
-                  </div>
-                  <h3 className='ml-1.5 text-base text-zinc-800 group-hover:text-blue-500'>
-                    Delete
-                  </h3>
-                </li>
-              </ul>
-            </DropdownMenuContent>
-          </DropdownMenu>
+  <>
+    <div className='mb-12 px-6 pt-24 md:pl-0 md:pr-6 md:pt-16'>
+      <div className='rounded-md bg-white pb-7 shadow-lg'>
+        <div className='border-b border-neutral-200 px-6 py-3'>
+          <h1 className='text-lg font-semibold text-zinc-800'>Grades</h1>
         </div>
+        <div className='p-4 sm:px-6 sm:pb-3 sm:pt-8'>
+          <Label
+            htmlFor='name'
+            className='mb-2 block text-sm font-normal leading-none text-zinc-800'
+          >
+            Grade
+          </Label>
+          <div className='flex flex-wrap items-center'>
+            <Input
+              id='name'
+              className='h-10 w-auto min-w-0 grow basis-0 rounded-lg border-neutral-200 px-4 text-base text-zinc-800 shadow-none placeholder:text-zinc-800 lg:h-12 lg:px-3 lg:py-2'
+              placeholder='Grade 1'
+            />
+            <DropdownMenu>
+              <DropdownMenuTrigger className='ml-4 flex h-8 w-8 items-center justify-center rounded-full border border-zinc-800'>
+                <GradeDots />
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className='absolute -right-4 top-4 rounded-md drop-shadow-xl'>
+                <ul className='m-0 px-2 py-3'>
+                  <li className='group mb-2 flex cursor-pointer items-center p-1.5'>
+                    <div className='text-zinc-800 group-hover:text-blue-500'>
+                      <Edit />
+                    </div>
+                    <h3 className='ml-1.5 text-base text-zinc-800 group-hover:text-blue-500'>
+                      Edit
+                    </h3>
+                  </li>
+                  <li className='group flex cursor-pointer items-center p-1.5'>
+                    <div className='text-zinc-800 group-hover:text-blue-500'>
+                      <Trash2 />
+                    </div>
+                    <h3 className='ml-1.5 text-base text-zinc-800 group-hover:text-blue-500'>
+                      Delete
+                    </h3>
+                  </li>
+                </ul>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
+        </div>
+        <Dialog>
+          <DialogTrigger asChild>
+            <div className='mx-auto flex max-w-80 justify-center'>
+              <Button className='h-12 w-60 text-base font-semibold sm:w-80'>Add New Grade</Button>
+            </div>
+          </DialogTrigger>
+          <DialogContent className='max-w-[620px]'>
+            <DialogHeader>
+              <DialogTitle className='text-center'>Add New Grade</DialogTitle>
+            </DialogHeader>
+            <div className='grid py-4'>
+              <Label className='mb-2 block text-sm font-normal leading-none text-zinc-800'>
+                Grade
+              </Label>
+              <div className='flex items-center'>
+                <Input
+                  id='name'
+                  className='h-10 rounded-lg border-neutral-200 px-4 text-base text-zinc-800 shadow-none placeholder:text-stone-300 lg:h-12 lg:px-3 lg:py-2'
+                  placeholder='Grade 1'
+                />
+              </div>
+            </div>
+            <DialogFooter>
+              <div className='w-1/2'>
+                <Button variant='outline' className='h-12 w-full text-base font-semibold'>
+                  Cancel
+                </Button>
+              </div>
+              <div className='w-1/2'>
+                <Button className='h-12 w-full text-base font-semibold'>Save</Button>
+              </div>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
       </div>
-      <Dialog>
-        <DialogTrigger asChild>
-          <div className='mx-auto flex max-w-80 justify-center'>
-            <Button className='h-12 w-80 text-base font-semibold'>Add New Grade</Button>
-          </div>
-        </DialogTrigger>
-        <DialogContent className='max-w-[620px]'>
-          <DialogHeader>
-            <DialogTitle className='text-center'>Add New Grade</DialogTitle>
-          </DialogHeader>
-          <div className='grid py-4'>
-            <Label className='mb-2 block text-sm font-normal leading-none text-zinc-800'>
-              Grade
-            </Label>
-            <div className='flex items-center'>
-              <Input
-                id='name'
-                className='h-10 rounded-lg border-neutral-200 px-4 text-base text-zinc-800 shadow-none placeholder:text-stone-300 lg:h-12 lg:px-3 lg:py-2'
-                placeholder='Grade 1'
-              />
-            </div>
-          </div>
-          <DialogFooter>
-            <div className='w-1/2'>
-              <Button variant='outline' className='h-12 w-full text-base font-semibold'>
-                Cancel
-              </Button>
-            </div>
-            <div className='w-1/2'>
-              <Button className='h-12 w-full text-base font-semibold'>Save</Button>
-            </div>
-          </DialogFooter>
-        </DialogContent>
-      </Dialog>
     </div>
-  </div>
+    <div className='px-6 md:px-0 md:pr-10'>
+      <div className='rounded-md bg-white p-4 shadow-lg lg:p-6'>
+        <Table>
+          <TableCaption>Showing 1 to 10 of 100 listings</TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead className='w-[93%]'>Grades</TableHead>
+              <TableHead className='border-l border-solid border-zinc-300'>Action</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {invoices.map((invoice) => (
+              <TableRow key={invoice.invoice}>
+                <TableCell className='font-base text-zinc-800'>{invoice.invoice}</TableCell>
+                <TableCell className='border-l border-solid border-zinc-300'>
+                  <i className='duration-400 inline-block cursor-pointer transition-all hover:text-primary'>
+                    {invoice.icon}
+                  </i>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+    </div>
+  </>
 );
