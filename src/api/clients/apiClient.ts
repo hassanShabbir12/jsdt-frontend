193ddde8b28b8c2fe9bf6bb18b6580c6aa5dbfef
Muhammad Mahad <mahadhashmi2@gmail.com>
2024-11-13 17:26:09 +0500

    fix(axios-setup): axios instance error

--- src/api/clients/apiClient.ts
+++ src/api/clients/apiClient.ts
@@ -1,10 +1,6 @@
-import type { AxiosInstance, InternalAxiosRequestConfig } from 'axios';
-
 import { JsdtAPI } from '@/lib/sdk/jsdt/Api';
 
-import { handleError } from '../config/errorHandler';
-import { retryRequest } from '../config/retryHandler';
-import { addPendingRequest, removePendingRequest } from './requestCache';
+import { setupInterceptors } from '../config/interceptors';
 
 export class ExtendedJsdtAPI extends JsdtAPI<unknown> {
   constructor() {
@@ -14,39 +10,7 @@ export class ExtendedJsdtAPI extends JsdtAPI<unknown> {
     });
 
     // Set up interceptors on the instance after creation
-    this.setupInterceptors(this.instance);
-  }
-
-  private setupInterceptors(axiosInstance: AxiosInstance): void {
-    axiosInstance.interceptors.request.use((config: InternalAxiosRequestConfig) => {
-      addPendingRequest(config);
-      // Set the Authorization token, if available
-      const token = localStorage.getItem('authToken');
-
-      if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-      }
-
-      return config;
-    });
-
-    axiosInstance.interceptors.response.use(
-      (response) => {
-        removePendingRequest(response.config);
-
-        return response;
-      },
-      (error) => {
-        removePendingRequest(error.config);
-
-        if (!error.response || error.code === 'ECONNABORTED') {
-          // Retry on network errors or timeouts
-          return retryRequest(() => axiosInstance.request(error.config));
-        }
-
-        return handleError(error);
-      },
-    );
+    setupInterceptors(this.instance);
   }
 }
 
