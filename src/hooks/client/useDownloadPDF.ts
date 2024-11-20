0e9a377673ad3e2eccffe80e465e25feaadd6d2f
muhammadmahad-debug <mahadhashmi2@gmail.com>
2024-11-20 18:37:12 +0500

    feat(pdf): questions answer download pdf

---
+++ src/hooks/client/useDownloadPDF.ts
@@ -0,0 +1,69 @@
+import { useState } from 'react';
+
+import axios from 'axios';
+
+import { apiClient } from '@/api/clients/apiClient';
+import { toast } from '@/hooks/use-toast';
+import { ExtendedCreateQuestionDto } from '@/interface/question';
+
+interface UseDownloadQuestionsReturn {
+  downloadQuestions: (questions: ExtendedCreateQuestionDto[], type: string) => Promise<void>;
+  loading: boolean;
+}
+
+export const useDownloadQuestions = (): UseDownloadQuestionsReturn => {
+  const [loading, setLoading] = useState<boolean>(false);
+  const downloadQuestions = async (
+    questions: ExtendedCreateQuestionDto[],
+    type: string,
+  ): Promise<void> => {
+    try {
+      if (!questions || questions.length === 0) {
+        toast({
+          title: 'Error',
+          description: 'No questions available to download.',
+        });
+
+        return;
+      }
+      setLoading(true);
+      const idArray = questions.map((item) => item.id);
+      const response = await apiClient.instance.post(
+        `/pdf/question?type=${type}`,
+        { items: idArray },
+        {
+          responseType: 'blob',
+        },
+      );
+      const pdfUrl = window.URL.createObjectURL(new Blob([response.data]));
+      const link = document.createElement('a');
+
+      link.href = pdfUrl;
+      link.setAttribute('download', 'custom-design.pdf');
+      document.body.appendChild(link);
+      link.click();
+      link.remove();
+      window.URL.revokeObjectURL(pdfUrl);
+      toast({
+        title: 'PDF Download',
+        description: 'Your PDF has been downloaded successfully.',
+      });
+    } catch (error) {
+      if (axios.isAxiosError(error) && error.response) {
+        toast({
+          title: 'Error',
+          description: error.response.data.message,
+        });
+      } else {
+        toast({
+          title: 'Error',
+          description: 'Failed to change password',
+        });
+      }
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { downloadQuestions, loading };
+};
--- src/pages/settings/index.tsx
+++ src/pages/settings/index.tsx
@@ -5,7 +5,7 @@ import { Eye, EyeOff } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
-import { useChangePassword } from '@/hooks/useChangePassword';
+import { useChangePassword } from '@/hooks/client/useChangePassword';
 
 export const Settings: FC = () => {
   const {
