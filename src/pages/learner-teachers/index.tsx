fdd94e46eb5a6252f37b31e4e550e7a9cc7ba42f
Amir Shahzad <112079597+Amir00786@users.noreply.github.com>
2024-11-21 15:18:43 +0500

    style(remove cn): remove cn styles

--- src/pages/learner-teachers/index.tsx
+++ src/pages/learner-teachers/index.tsx
@@ -40,7 +40,6 @@ import { useTopicList } from '@/hooks/admin/topic/useTopicList';
 import { useDownloadQuestions } from '@/hooks/client/useDownloadPDF';
 import { useInvestigation } from '@/hooks/client/useInvestigation';
 import { assetUrl } from '@/lib/asset-url';
-import { cn } from '@/lib/utils';
 import { calculatePercentage, calculateTotalMarks } from '@/utils/helper';
 
 export const LearnerTeacher: FC = () => {
@@ -840,9 +839,7 @@ export const LearnerTeacher: FC = () => {
                                 variant='outline'
                                 className='group flex h-12 w-full items-center justify-between border border-solid border-neutral-200 px-4 py-2 font-normal text-stone-300 shadow-none hover:bg-transparent'
                               >
-                                <span
-                                  className={cn('text-stone-300', !date ? '' : 'text-zinc-800')}
-                                >
+                                <span className={`text-stone-300 ${!date ? '' : 'text-zinc-800'}`}>
                                   {date ? formatDate(date) : 'DD-MM-YYYY'}
                                 </span>
                                 <span className='text-stone-300 group-hover:text-stone-300'>
