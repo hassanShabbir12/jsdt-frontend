b4efaeac90a7945e7e5001684a974742c9429df1
muhammadmahad-debug <mahadhashmi2@gmail.com>
2024-12-06 18:41:08 +0500

    fix(question): add editor in question module

---
+++ src/pages/question/quill-editor/index.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import ReactQuill from 'react-quill';
+
+interface QuillEditorProps {
+  onChange: (content: string) => void; // Callback to handle content changes
+  placeholder?: string;
+  value: string;
+}
+
+const QuillEditor: React.FC<QuillEditorProps> = ({ onChange, placeholder, value }) => {
+  const handleChange = (content: string): void => {
+    onChange(content);
+  };
+
+  const modules = {
+    toolbar: [
+      [{ header: [1, 2, false] }],
+      ['bold', 'italic', 'underline', 'strike'],
+      [{ script: 'sub' }, { script: 'super' }],
+      [{ list: 'ordered' }, { list: 'bullet' }],
+      //   ['link', 'image', 'video'],
+      [{ color: [] }, { background: [] }],
+      [{ align: [] }],
+      ['clean'],
+    ],
+  };
+
+  // Define the formats you want to support
+  const formats = [
+    'header',
+    'bold',
+    'italic',
+    'underline',
+    'strike',
+    'script',
+    'list',
+    'bullet',
+    'link',
+    'image',
+    'video',
+    'color',
+    'background',
+    'align',
+  ];
+
+  return (
+    <div className='relative overflow-hidden rounded-md border border-input focus-within:border-blue-700'>
+      <ReactQuill
+        onChange={handleChange}
+        placeholder={placeholder}
+        modules={modules}
+        formats={formats}
+        value={value}
+        theme='snow'
+      />
+    </div>
+  );
+};
+
+export default QuillEditor;
