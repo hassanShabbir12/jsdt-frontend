c0588a1e8d19728564ed48cbbca78fe98b7cac36
hassanShabbir12 <syedhassanshabbir14@gmail.com>
2025-02-26 23:46:03 +0500

    feat(linting): added important eslint prettier setting

--- eslint.config.js
+++ eslint.config.js
@@ -95,7 +95,10 @@ export default tseslint.config(
       'no-nested-ternary': 'error',
       '@typescript-eslint/explicit-function-return-type': 'error',
       '@typescript-eslint/no-explicit-any': 'error',
-      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+      ],
       '@typescript-eslint/no-non-null-assertion': 'error',
       '@typescript-eslint/no-use-before-define': [
         'error',
